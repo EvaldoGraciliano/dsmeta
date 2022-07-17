103b8776932d0b26528805b6a97d5d55356041cb
EvaldoGraciliano <neto.graciliano@hotmail.com>
2022-07-17 00:28:51 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmeta/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmeta;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DsmetaApplication.class);
+	}
+
+}
