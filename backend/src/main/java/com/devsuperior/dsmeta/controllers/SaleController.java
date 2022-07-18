7c35a40446035174d15615de25c2646903b13948
EvaldoGraciliano <neto.graciliano@hotmail.com>
2022-07-18 15:41:01 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 
 @RestController
 @RequestMapping(value="/sales")
@@ -22,6 +24,9 @@ public class SaleController {
 	
 	@Autowired
 	SaleService service;
+	
+	@Autowired
+	SmsService smsService;
 	
 	@GetMapping
 	public Page<Sale> findSales(
@@ -37,6 +42,11 @@ public class SaleController {
 	public List<Sale> findSales(){
 			
 		return  service.findall();
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable long id) {
+		smsService.sendSms(id);
 	}
 	
 	
--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 
 
 
