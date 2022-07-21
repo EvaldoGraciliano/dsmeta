9692356cd4a4f65330e8e7ebc5fdf36c57fdd54f
EvaldoGraciliano <neto.graciliano@hotmail.com>
2022-07-21 01:13:33 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from "../NotificationButton";
@@ -11,6 +12,13 @@ function SalesCard() {
 
   const [minDate,setMinDate] = useState(new Date());
   const [maxDate, setMaxDate] = useState(new Date());
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/sales")
+    .then(response => {
+        console.log(response.data)
+     });
+  }, []);
 
   return (
     <div className="dsmeta-card">
@@ -95,3 +103,7 @@ function SalesCard() {
 }
 
 export default SalesCard;
+function UseEffect() {
+  throw new Error("Function not implemented.");
+}
+
